c47f09be87e5ad5224b967c2ea5cd1e3b88e7b35
yokopaulishii <92817049+yokopaulishii@users.noreply.github.com>
2022-09-20 15:59:07 +0200

    Create main_test.py

---
+++ main_test.py
@@ -0,0 +1,36 @@
+import pytest
+
+from main import app
+
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client
+
+
+def test_redirect(client):
+    response = client.get("/home")
+    assert response.status_code == 302
+    assert response.location == "http://localhost/"
+
+
+def test_index(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"<title>Index</title>" in response.data
+
+
+def test_about(client):
+    response = client.get("/about")
+    assert response.status_code == 200
+    assert b"<title>About</title>" in response.data
+
+
+""" Write your own tests below."""
+
+def test_third(client):
+    response = client.get("/third")
+    assert response.status_code == 200
+    assert b"<title>Third</title>" in response.data
