49f8f9077ac147d1a86ae240c087a98a3fc56e30
yokopaulishii <92817049+yokopaulishii@users.noreply.github.com>
2022-09-20 13:48:11 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, redirect, url_for, request
+from markupsafe import escape
+from jinja2 import Template
+
+__winc_id__ = "9263bbfddbeb4a0397de231a1e33240a"
+__human_name__ = "templates"
+
+app = Flask(__name__)
+
+
+@app.route("/home", methods= ['GET'])
+def home():
+    if request.method == ['GET']:
+        return redirect (302, '/')
+
+@app.route("/", methods= ['GET'])
+def index(title):
+    if request.method == ['GET']:
+        return f'render_template(index.html, {escape(title):Index})'
+
+@app.route("/about", methods= ['GET'])
+def about(title):
+    if request.method == ['GET']:
+        return f'render_template(about.html, {escape(title):About})'
+
+@app.route("/third", methods= ['GET'])
+def third(title):
+    if request.method == ['GET']:
+        return f'render_template(third.html, {escape(title):Third})'
+
+if __name__=='__main__':
+    app.run(debug=True)
